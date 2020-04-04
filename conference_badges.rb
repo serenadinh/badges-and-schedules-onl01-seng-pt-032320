a0b85ca87cb45bdec301ab8adae2f95de84f3b4f
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-04-04 20:30:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,19 +1,22 @@
 # Write your code here.
 
-def badge_maker (name)
+def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (speakers)
+def batch_badge_creator(speakers)
+  # returns an array of badge messages, using badge_maker
   badge_messages = []
   speakers.each do |speaker|
     message = badge_maker(speaker)
-    badge_messages << messages
+    badge_messages << message
   end
   badge_messages
 end
 
 def assign_rooms(speakers)
+  # assigns each speaker to a room, rooms 1-7.
+  # return a list of room assignments in the form of: "Hello____! You'll be assigned to room ___!"
   room_number = 1
   room_messages = []
   speakers.each do |speaker|
@@ -23,13 +26,14 @@ def assign_rooms(speakers)
   room_messages
 end
 
-def printer (attendees)
-  badge_messages = batch_badge_creator (speakers)
+def printer(speakers)
+  # outputs the results of batch_badge_creator, and assign_rooms
+  badge_messages = batch_badge_creator(speakers)
   badge_messages.each do |message|
     puts message
-end
-  room_messages = assign_rooms (speakers)
+  end
+  room_messages = assign_rooms(speakers)
   room_messages.each do |message|
     puts message
-end
+  end
 end
